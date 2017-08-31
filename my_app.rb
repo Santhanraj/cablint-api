abd94b2ad1255ec5bcab7ae59e89754cb7575f4c
santhan <santhanr@gmail.com>
2017-08-31 06:24:08 +0000

    JSON in JSON out

--- my_app.rb
+++ my_app.rb
@@ -5,8 +5,10 @@ class MyApp < Sinatra::Base
   before do
       content_type :json
   end
+
+
   post "/" do
-      payload = JSON.parse(request.body.read).symbolize_keys
+      payload = JSON.parse(request.body.read, :symbolize_keys => true)
       { serial_no: payload[:serial_no], success: "1"}.to_json
   end
 end
