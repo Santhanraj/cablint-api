090e4332bd028f078c2dc55e61f6d8a0e092ff23
santhan <santhanr@gmail.com>
2017-08-31 05:35:28 +0000

    initial push

---
+++ my_app.rb
@@ -0,0 +1,12 @@
+require 'sinatra/base'
+require 'json'
+
+class MyApp < Sinatra::Base
+  before do
+      content_type :json
+  end
+  post "/" do
+      payload = JSON.parse(request.body.read).symbolize_keys
+      { serial_no: payload[:serial_no], success: "1"}.to_json
+  end
+end
