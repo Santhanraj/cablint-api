f2147100491e6079d6e78064794e10a918a363e4
santhan <santhanr@gmail.com>
2017-09-01 03:15:03 +0000

    certlint with

--- my_app.rb
+++ my_app.rb
@@ -1,14 +1,24 @@
 require 'sinatra/base'
 require 'json'
+require 'openssl'
 
 class MyApp < Sinatra::Base
   before do
-      content_type :json
+      	content_type :json
   end
 
 
   post "/" do
-      payload = JSON.parse(request.body.read, :symbolize_keys => true)
-      { serial_no: payload[:serial_no], success: "1"}.to_json
+      	payload = JSON.parse(request.body.read, :symbolize_names => true)
+	raw = payload[:pem]
+	sub_CN =""
+	pem = OpenSSL::X509::Certificate.new raw	
+	pem.subject.to_a.each do |attri|
+        	sub_CN = attri[1].to_s if attri[0] == "CN"
+    	end	
+      { message: sub_CN, success: "1" }.to_json
+
+
+
   end
 end
