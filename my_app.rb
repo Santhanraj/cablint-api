f0e081e23420edac84560cc30d664812675796aa
santhan <santhanr@gmail.com>
2017-09-03 04:15:51 +0000

    working version with cablint integration and error handling

--- my_app.rb
+++ my_app.rb
@@ -1,6 +1,19 @@
+lib = File.expand_path("../../certlint/lib", __FILE__)
+ext = File.expand_path("../../certlint/ext", __FILE__)
+$:.unshift(lib)
+$:.unshift(ext)
+
+
+require 'certlint'
 require 'sinatra/base'
 require 'json'
 require 'openssl'
+require 'certlint/version'
+require 'certlint/certlint'
+require 'certlint/cablint'
+require 'certlint/pemlint'
+require 'certlint/namelint'
+require 'certlint/generalnames'
 
 class MyApp < Sinatra::Base
   before do
@@ -12,12 +25,16 @@ class MyApp < Sinatra::Base
       	payload = JSON.parse(request.body.read, :symbolize_names => true)
 	raw = payload[:pem]
 	sub_CN =""
-	pem = OpenSSL::X509::Certificate.new raw	
-	pem.subject.to_a.each do |attri|
-        	sub_CN = attri[1].to_s if attri[0] == "CN"
-    	end
-	File.open("cert.cer", "wb") { |f| f.print pem }
-	message = %x[ruby ../certlint/bin/cablint test.cer]	
-      {subject: sub_CN, message: message, success: "1" }.to_json
+	begin
+		pem = OpenSSL::X509::Certificate.new raw	
+		pem.subject.to_a.each do |attri|
+        		sub_CN = attri[1].to_s if attri[0] == "CN"
+    		end
+        	m, der = CertLint::PEMLint.lint(raw, 'CERTIFICATE')
+        	m += CertLint::CABLint.lint(der)
+        	{subject: sub_CN, message: m, success: "1" }.to_json
+	rescue StandardError=>e
+		{subject: "", message: e, success: "0" }.to_json	
+	end
   end
 end
